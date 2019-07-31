65966a0a702ce00182bfa828da4f7d3338393a9a
Lúcia Emília <luciaemilia72@gmail.com>
2019-07-31 08:27:16 -0300

    Correção de erros e ajuste de entradas e saídas

--- interLabel.py
+++ interLabel.py
@@ -12,17 +12,23 @@ class LABELROTULATOR(object):
 		self.X = base.drop(['classe'],axis = 1).get_values()
 		self.Y = KMeans(n_clusters=num_cluster, random_state=0).fit(self.X).labels_
 		
+		data = pd.DataFrame(self.X)
+		data.loc[:,'Cluster'] = pd.Series(self.Y, index=data.index)
+		self.baseagrupada = data.get_values()
+		
 		# Rotulaçaõ do conjunto de treino e seleção dos elementos pertencentes ao rótulo 
 		# dados -> componente x dos elementos pertencentes ao rótulo
 		# cluster -> componente y dos elementos pertencentes ao rótulo
 		# notDados -> elementos não pertencentes ao rótulo
 		# rognData -> elementos que um cluster que não obedecem ao rótulo deste
 		# rongCluster -> tuplas de elementos e clusters para os quais o elemento obedece ao rótulo
+		
 		self.rotulos = rotulador.Rotulador(self.X, self.Y, discre_method, discre_bins, perc_trei_rot, V, folds_rot).rotulo
-		self.dados, self.cluster, self.notDados, self.rongData, self.rongcluster = self.selectData()
+		for i in self.rotulos: print(i)
+		self.dados, self.notDados, self.rongData,  self.rongcluster, self.cluster = self.selectData()
 
 	def selectData(self):
-		newData=[]
+		rightData=[]
 		notData=[]
 		rongData=[]
 		rongcluster = []
@@ -32,26 +38,29 @@ class LABELROTULATOR(object):
 		grouped = data.groupby(['Cluster'])
 
 		for n_cluster, values in grouped:
-			for i in values.get_values().tolist():
-				for j in self.rotulos:
-					if n_cluster == j[0]:
-						if self.check(i, j[1]):
-							newData.append(i)
-						else: notData.append(i)
+			for dado in values.get_values().tolist():
+				for rotulo in self.rotulos:
+					if self.check(dado, rotulo[1]):
+						if rotulo[0] == n_cluster: 
+							rightData.append(dado)
+						else:
+							rongData.append(dado)
+							rongcluster.append((dado,rotulo[0]))
 					else:
-						if self.check(i, j[1]):
-							rongData.append(i)
-							rongcluster.append((i,j[0]))	
-
-		cluster = [i[-1] for i in newData]
-		return np.asarray(newData), np.asarray(cluster), np.asarray(notData), np.asarray(rongData), rongcluster
+						if rotulo[0] == n_cluster: notData.append(dado)
+		
+		cluster = [i[-1] for i in rightData]
+		return np.asarray(rightData), np.asarray(notData), np.asarray(rongData), rongcluster, np.asarray(cluster)
 	
-	def check(self, dado, rotulos): 	 	
-		for j in rotulos:
-			for i in range(len(dado)):
-				if all([i == j[0], dado[i] >= j[1], dado[i] <= j[2]]):
-					return True
-		return False
+	def check(self, dado, rotulos):
+		#print(dado, rotulos) 
+		for rotulo in rotulos:
+			if all([dado[rotulo[0]] >= rotulo[1], dado[rotulo[0]] <= rotulo[2]]): 
+				pass
+			else: 
+				return False
+		return True 
+		
 
 
 
--- script.py
+++ script.py
@@ -2,21 +2,44 @@ import interLabel as label
 import pandas as pd
 import numpy as np 
 
-chamadas = ["./databases/sementes.csv",3, [3,3,3,3,3,3,3], 5]
+def comparaListas(list1, list2):
+	for i in range(len(list1)):
+		if list1[i]!=list2[i]: return False
+	return True
+
+def procurarElemento(elemento, lista):
+	for i in lista:
+		if comparaListas(elemento, i): return True
+	return False
+
+chamadas = ["./databases/iris.csv",3,"EFD", [3,3,3,3], 10]
 bd = pd.read_csv(chamadas[0],sep=',',parse_dates=True)
-rotular = label.LABELROTULATOR(bd, chamadas[1], "EFD", chamadas[2], 60, chamadas[3], 1)
+rotular = label.LABELROTULATOR(bd, chamadas[1], chamadas[2], chamadas[3], 60, chamadas[4], 10)
 
+'''
+"./databases/iris.csv","EFD", 4, [3,3,3,3], 10"
+("./databases/vidros.csv","EWD",7, [4,4,4,4,4,4,4,4,4],15),
+("./databases/sementes.csv","EFD",3, [3,3,3,3,3,3,3], 5)'''
+naoObedeceOutrosRotulos = [x for x in rotular.baseagrupada if not procurarElemento(x,rotular.rongData)]
 
-'''("./databases/vidros.csv",7, [4,4,4,4,4,4,4,4,4],15),
-("./databases/sementes.csv",3, [3,3,3,3,3,3,3], 5)'''
+print("tamanho da base: "+str(len(rotular.X)))
+print("Rotulados certos: "+str(len(rotular.dados)))
+print("Nao obedecem ao rotulo: "+str(len(rotular.notDados)))
+print("Obedecem a outros rotulos: "+str(len(rotular.rongData)))
+print("Nao obedecem a outros rotulos: "+str(len(naoObedeceOutrosRotulos)))
 
+maisDeUmGrupo = [x for x in rotular.rongData for y in rotular.dados if comparaListas(x,y)]
+print("Mais de um grupo: " +str(len(maisDeUmGrupo)))
+#for i in maisDeUmGrupo: print(i)
 
-print("Mais de um cluster:")
-for i in rotular.rongcluster: print(i[0], i[1])
+grupoErrado = [x for x in rotular.notDados for y in rotular.rongData if comparaListas(x,y)]
+print("Grupo errado: "+str(len(grupoErrado)))
+for i in grupoErrado: print(i)
 
-print("\nNão pertence ao rótulo:")
-print(rotular.notDados)
+exclusivo = [x for x in rotular.dados for y in naoObedeceOutrosRotulos if comparaListas(x,y)]
+print("Exclusivo: "+str(len(exclusivo)))
+#for i in exclusivo: print(i)
 
-print("\nPertence a outro grupo:")
-lista_final = [x for x in rotular.notDados if x in rotular.rongData]
-print(np.asarray(lista_final))
+erro = [x for x in rotular.notDados for y in naoObedeceOutrosRotulos if comparaListas(x,y)]
+print("Erro: "+str(len(erro)))
+#for i in erro: print(i)
