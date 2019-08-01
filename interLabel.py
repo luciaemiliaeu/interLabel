91c17af1cb28baf8f4f5baa103d386587f9c5c51
luciaemiliaeu <luciaemilia72@gmail.com>
2019-08-01 15:58:23 -0300

    Correção dos grupos

--- interLabel.py
+++ interLabel.py
@@ -11,52 +11,84 @@ class LABELROTULATOR(object):
 		#Agurpamento
 		self.X = base.drop(['classe'],axis = 1).get_values()
 		self.Y = KMeans(n_clusters=num_cluster, random_state=0).fit(self.X).labels_
-		
 		data = pd.DataFrame(self.X)
 		data.loc[:,'Cluster'] = pd.Series(self.Y, index=data.index)
 		self.baseagrupada = data.get_values()
 		
-		# Rotulaçaõ do conjunto de treino e seleção dos elementos pertencentes ao rótulo 
-		# dados -> componente x dos elementos pertencentes ao rótulo
-		# cluster -> componente y dos elementos pertencentes ao rótulo
-		# notDados -> elementos não pertencentes ao rótulo
-		# rognData -> elementos que um cluster que não obedecem ao rótulo deste
-		# rongCluster -> tuplas de elementos e clusters para os quais o elemento obedece ao rótulo
-		
+		#Rotulação
 		self.rotulos = rotulador.Rotulador(self.X, self.Y, discre_method, discre_bins, perc_trei_rot, V, folds_rot).rotulo
 		for i in self.rotulos: print(i)
-		self.dados, self.notDados, self.wrongData,  self.wrongcluster = self.selectData()
+		
+		# Mudança dos elementos de cluster
+		self.otherCluster, self.multLabel, self.wrongCluster, self.exclusive, self.error = self.calMetrics(self.X, self.Y, self.rotulos)
+		self.newLabels, self.newRightData = self.changeLabel(self.otherCluster, self.wrongCluster)
+		newData = pd.DataFrame(self.multLabel+self.exclusive+self.error+self.newRightData)
+		newcluster = newData.loc[:, newData.shape[1]-1].get_values()
+		newX = newData.drop(newData.columns[[newData.shape[1]-1]], axis=1).get_values()
 
-		self.naoObedeceOutrosRotulos = [x for x in self.baseagrupada if not self.procurarElemento(x,self.wrongData)]
-		self.maisDeUmGrupo = [x for x in self.wrongData for y in self.dados if self.comparaListas(x,y)]
-		self.grupoErrado = [x for x in self.notDados for y in self.wrongData if self.comparaListas(x,y)]
-		self.exclusivo = [x for x in self.dados for y in self.naoObedeceOutrosRotulos if self.comparaListas(x,y)]
-		self.erro = [x for x in self.notDados for y in self.naoObedeceOutrosRotulos if self.comparaListas(x,y)]
-		self.changeLabel(self.wrongcluster, self.grupoErrado)
+		#Repete a rotulação 
+		newRotulos = rotulador.Rotulador(newX, newcluster, discre_method, discre_bins, perc_trei_rot, V, folds_rot).rotulo
+		for i in newRotulos: print(i)
+
+		#Recalcula métricas
+		a,b,c,d,e = self.calMetrics(newX, newcluster, newRotulos)
+
+	def calMetrics(self, X, Y, rotulos):
+		#Separação dos dados (Matriz de confusão)
+		rightLabel, wrongLabel, otherClusterData,  otherCluster, noOtherCluster = self.selectData(X,Y,rotulos)
 	
-	def selectData(self):
-		rightData=[]
-		notData=[]
-		wrongData=[]
-		wrongcluster = []
+		multLabel = [x for x in otherClusterData if self.procurarElemento(x,rightLabel)]
+		wrongCluster = [x for x in wrongLabel if self.procurarElemento(x,otherClusterData)]
+		exclusive = [x for x in rightLabel  if self.procurarElemento(x,noOtherCluster)]
+		error = [x for x in wrongLabel if self.procurarElemento(x,noOtherCluster)]
 
-		data = pd.DataFrame(self.X)
-		data.loc[:,'Cluster'] = pd.Series(self.Y, index=data.index)
+		print("tamanho da base: "+str(len(X)))
+		print("Rotulados certos: "+str(len(rightLabel)))
+		print("Nao obedecem ao rotulo: "+str(len(wrongLabel)))
+		print("Obedecem a outros rotulos: "+str(len(otherClusterData)))
+		print("Nao obedecem a outros rotulos: "+str(len(noOtherCluster)))
+
+		print("Mais de um grupo: " +str(len(multLabel)))
+		#for i in rotular.multLabel: print(i)
+
+		print("Grupo errado: "+str(len(wrongCluster)))
+		#for i in rotular.wrongCluster: print(i)
+
+		print("Exclusivo: "+str(len(exclusive)))
+		#for i in rotular.exclusive: print(i)
+
+		print("Erro: "+str(len(error)))
+		#for i in rotular.error: print(i)
+
+
+		return otherCluster, multLabel, wrongCluster, exclusive, error
+	
+	def selectData(self, X, Y, rotulos):
+		rightLabel=[]
+		wrongLabel=[]
+		
+		otherClusterData=[]
+		otherCluster = []
+
+		data = pd.DataFrame(X)
+		data.loc[:,'Cluster'] = pd.Series(Y, index=data.index)
 		grouped = data.groupby(['Cluster'])
 
 		for n_cluster, values in grouped:
 			for dado in values.get_values().tolist():
-				for rotulo in self.rotulos:
+				for rotulo in rotulos:
 					if self.check(dado, rotulo[1]):
 						if rotulo[0] == n_cluster: 
-							rightData.append(dado)
+							rightLabel.append(dado)
 						else:
-							wrongData.append(dado)
-							wrongcluster.append((dado,rotulo[0]))
+							otherClusterData.append(dado)
+							otherCluster.append((dado,rotulo[0]))
 					else:
-						if rotulo[0] == n_cluster: notData.append(dado)
+						if rotulo[0] == n_cluster: wrongLabel.append(dado)
+
+		notOtherCluster = [x for x in self.baseagrupada if not self.procurarElemento(x,otherClusterData)]
 		
-		return np.asarray(rightData), np.asarray(notData), np.asarray(wrongData), wrongcluster
+		return np.asarray(rightLabel), np.asarray(wrongLabel), np.asarray(otherClusterData), otherCluster, notOtherCluster
 	
 	def check(self, dado, rotulos):
 		#print(dado, rotulos) 
@@ -68,25 +100,24 @@ class LABELROTULATOR(object):
 		return True 
 	
 	def changeLabel(self, dados, grupoErrado):
-		#for i in dados: print(i)
-		#print("\n")
-		#for i in grupoErrado: print(i)
-
-		for y in dados:
-			if self.procurarElemento(y[0], grupoErrado):
-				y[0][-1]=y[1]
-				#print(y[0])
-		
+		for x in dados:
+			for y in grupoErrado:
+				if self.comparaListas(x[0],y):
+					x[0][-1]=x[1]
+					y[-1]=x[1]
+		return dados, grupoErrado
+	
+	#Procura uma lista (elemento) em uma lista de listas (lista)
+	def procurarElemento(self, elemento, lista):
+		for i in lista:
+			if self.comparaListas(elemento, i): return True
+		return False
 
 	def comparaListas(self, list1, list2):
 		for i in range(len(list1)):
 			if list1[i]!=list2[i]: return False
 		return True
 
-	def procurarElemento(self, elemento, lista):
-		for i in lista:
-			if self.comparaListas(elemento, i): return True
-		return False
 
 
 
--- script.py
+++ script.py
@@ -13,20 +13,3 @@ rotular = label.LABELROTULATOR(bd, chamadas[1], chamadas[2], chamadas[3], 60, ch
 ("./databases/sementes.csv","EFD",3, [3,3,3,3,3,3,3], 5)'''
 
 
-print("tamanho da base: "+str(len(rotular.X)))
-print("Rotulados certos: "+str(len(rotular.dados)))
-print("Nao obedecem ao rotulo: "+str(len(rotular.notDados)))
-print("Obedecem a outros rotulos: "+str(len(rotular.wrongData)))
-print("Nao obedecem a outros rotulos: "+str(len(rotular.naoObedeceOutrosRotulos)))
-
-print("Mais de um grupo: " +str(len(rotular.maisDeUmGrupo)))
-for i in rotular.maisDeUmGrupo: print(i)
-
-print("Grupo errado: "+str(len(rotular.grupoErrado)))
-for i in rotular.grupoErrado: print(i)
-
-print("Exclusivo: "+str(len(rotular.exclusivo)))
-#for i in exclusivo: print(i)
-
-print("Erro: "+str(len(rotular.erro)))
-for i in rotular.erro: print(i)
